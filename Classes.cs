77a5d58f1adef974334d8096442b15ab45c4a7fe
Jun Z <quietkeeper@users.noreply.github.com>
2017-12-08 14:41:33 -0500

    code re-org
    create runable project except solution file

---
+++ Classes.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ListNode
+    {
+        public ListNode(int value)
+        {
+            val = value;
+        }
+        public ListNode(int value,ListNode node)
+        {
+            val = value;
+            next = node;
+        }
+        public ListNode(string value)
+        {
+            strValue = value;
+        }
+        public ListNode(string value, ListNode node)
+        {
+            strValue = value;
+            next = node;
+        }
+        public ListNode next;
+        public int val;
+        public string strValue;
+    }
+    public class TreeNode
+    {
+        public TreeNode(int value)
+        {
+            val = value;
+        }
+
+        public TreeNode(string value)
+        {
+            strValue = value;
+        }
+        public TreeNode left;
+        public TreeNode right;
+        public int val;
+        public string strValue;
+    }
+
+   // class 
+}
--- Helper.cs
+++ Helper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LeetCode;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
